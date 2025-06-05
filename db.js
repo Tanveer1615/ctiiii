971eba4016f3acaa6d14e900ee394525c4b690b0
Tanveer <tanveeransari5663@gmail.com>
2025-06-05 10:44:05 +0530

    tanveer

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/BookMyPlanner");
+    console.log(`MongoDB Connected successfully....`);
+  } catch (err) {
+    console.error(`MongoDB Connection Failed:`, err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
